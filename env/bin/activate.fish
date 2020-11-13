344cffef5a6030504c6c9a0df1828acb5dc1084d
Mihail Piskunov <piskunov.m.a@gmail.com>
2020-11-13 20:49:33 +0300

    add gitignore

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/dmitrii/Документы/project_with_Dima/env"
+set -gx VIRTUAL_ENV "/home/mihail/Документы/geekshop/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
