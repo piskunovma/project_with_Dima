ffb99864197c188a318cf3bc119aeb5cca80ddc2
Михаил Пискунов <piskunov.m.a@gmail.com>
2021-01-09 22:20:16 +0300

    Добавлены картинки продуктов на главной cd /home/miha/Документы/project_with_Dima/carservice ; /usr/bin/env /home/miha/Документы/project_with_Dima/venv/bin/python /home/miha/.vscode/extensions/ms-python.python-2020.12.424452561/pythonFiles/lib/python/debugpy/launcher 46101 -- /home/miha/Документы/project_with_Dima/carservice/manage.py runserver 0.0.0.0:8000

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
 
 def main(request):
-    return render(request, "mainapp/index.html")
+    title = 'Главная'
+    products = [
+        {
+            'name': 'Шины',
+            'desc': 'Новые',
+            'image_src': 'tires.jpg',
+            'image_href': '/product/1/',
+            'alt': 'продукт 1',
+        },
+        {
+            'name': 'Двигатель',
+            'desc': 'Ремонт Двигателя',
+            'image_src': 'dvigatel.jpg',
+            'image_href': '/product/2/',
+            'alt': 'продукт 2',
+        },
+        {
+            'name': 'Масло',
+            'desc': 'Моторное',
+            'image_src': 'oil.jpg',
+            'image_href': '/product/3/',
+            'alt': 'продукт 3',
+        },
+        {
+            'name': 'КПП',
+            'desc': 'Коробка передач ремонт',
+            'image_src': 'dsg7_KPP.jpg',
+            'image_href': '/product/4/',
+            'alt': 'продукт 4',
+        },
+        {
+            'name': 'Диагностика',
+            'desc': 'Компьютерная',
+            'image_src': 'diagnostics.jpg',
+            'image_href': '/product/5/',
+            'alt': 'продукт 5',
+        },
+    ]
+    content = {"title": title, "products": products}
+    return render(request, "mainapp/index.html", content)
 
 def catalog(request):
     return render(request, "mainapp/catalog.html")
